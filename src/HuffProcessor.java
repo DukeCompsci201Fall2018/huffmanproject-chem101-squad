61f699cae957a8da65d3eface3d11f6e8cb4e36e
BrycenRushing <bcr16@duke.edu>
2018-12-06 19:07:50 -0500

    blah

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -1,3 +1,4 @@
+import java.util.PriorityQueue;
 
 /**
  * Although this class has a history of several years, it is starting from a
@@ -38,14 +39,48 @@ public class HuffProcessor {
 	 * @param out Buffered bit stream writing to the output file.
 	 */
 	public void compress(BitInputStream in, BitOutputStream out) {
+		int[] counts = readForCounts(in);
+		HuffNode root = makeTreeFromCounts(counts);
+		String[] codings = makeCodingsFromTree;
 
-		while (true) {
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1)
-				break;
-			out.writeBits(BITS_PER_WORD, val);
-		}
+		out.writeBits(BITS_PER_INT, HUFF_TREE);
+		writeHeader(root, out);
+
+		in.reset();
+		writeCompressedBits(codings, in, out);
 		out.close();
+	}
+
+	private HuffNode makeTreeFromCounts(int[] counts) {
+		PriorityQueue<HuffNode> pq = new PriorityQueue<>();
+		
+		for (int i = 0; i < counts.length ; i++ ) {
+			if (counts[i] > 0) {
+				pq.add(new HuffNode(i,counts[i],null,null));
+			}
+		}
+
+		while (pq.size() > 1) {
+		    HuffNode left = pq.remove();
+		    HuffNode right = pq.remove();
+		    HuffNode t = new HuffNode( , left.myWeight + right.myWeight, null, null)// create new HuffNode t with weight from
+		    // left.weight+right.weight and left, right subtrees
+		    pq.add(t);
+		}
+		HuffNode root = pq.remove();
+		return root;
+	}
+
+	private int[] readForCounts(BitInputStream in) {
+		int[] freq = new int[ALPH_SIZE + 1];
+		while (true) {
+			int index = in.readBits(BITS_PER_WORD);
+			if (index == -1)
+				break;
+			freq[index] = freq[index] + 1;
+		}
+		freq[PSEUDO_EOF] = 1;
+		return freq;
 	}
 
 	/**
@@ -63,11 +98,6 @@ public class HuffProcessor {
 		}
 		HuffNode root = readTreeHeader(in);
 		readCompressedBits(root, in, out);
-//		while (true){
-//			int val = in.readBits(BITS_PER_WORD);
-//			if (val == -1) break;
-//			out.writeBits(BITS_PER_WORD, val);
-//		}
 		out.close();
 	}
 
